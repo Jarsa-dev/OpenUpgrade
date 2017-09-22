f668915c67dc336a6a43510481dcd8e659d4ba6b
lmgr0312 <lmiguel.gruiz@gmail.com>
2017-09-22 09:39:43 -0500

    [Hiperauto] Fixes UoM & Set correct values of Stock Inventory Line and Stock Moves from pre-migration

--- openerp/addons/base/migrations/8.0.1.3/pre-migration.py
+++ openerp/addons/base/migrations/8.0.1.3/pre-migration.py
@@ -34,6 +34,7 @@ xml_ids = [
 def cleanup_modules(cr):
     """Don't report as missing these modules, as they are integrated in
     other modules."""
+
     openupgrade.update_module_names(
         cr, [
             # from OCA/product-attribute
@@ -49,6 +50,321 @@ def cleanup_modules(cr):
 @openupgrade.migrate()
 def migrate(cr, version):
     # Drop view that inhibits changing field types. It will be recreated BTW
+
+    # PIEZA, PIEZA & P to Unit(s) product_template
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uom_id = 1
+        WHERE uom_id in (16, 41, 42);
+        """)
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uos_id = 1
+        WHERE uos_id in (16, 41, 42);
+        """)
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uom_po_id = 1
+        WHERE uom_po_id in (16, 41, 42);
+        """)
+     # PIEZA, PIEZA & P to Unit(s) procurement_order
+    openupgrade.logged_query(
+        cr, """
+        UPDATE procurement_order
+        SET product_uom = 1
+        WHERE product_uom in (16, 41, 42);
+        """)
+    # PIEZA, PIEZA & P to Unit(s) purchase order line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE purchase_order_line
+        SET product_uom = 1
+        WHERE product_uom in (16, 41, 42);
+        """)
+    # PIEZA, PIEZA & P to Unit(s) sale order line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE sale_order_line
+        SET product_uom = 1
+        WHERE product_uom in (16, 41, 42);
+        """)
+    # PIEZA, PIEZA & P to Unit(s) stock inventory line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE stock_inventory_line
+        SET product_uom = 1
+        WHERE product_uom in (16, 41, 42);
+        """)
+    # PIEZA, PIEZA & P to Unit(s) stock move
+    openupgrade.logged_query(
+        cr, """
+        UPDATE stock_move
+        SET product_uom = 1
+        WHERE product_uom in (16, 41, 42);
+        """)
+    # SERVICIO, SERVICIO to Servicio product_template
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uom_id = 19
+        WHERE uom_id in (15, 40);
+        """)
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uos_id = 19
+        WHERE uos_id in (15, 40);
+        """)
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uom_po_id = 19
+        WHERE uom_po_id in (15, 40);
+        """)
+     # SERVICIO, SERVICIO & Servicio to Unit(s) procurement_order
+    openupgrade.logged_query(
+        cr, """
+        UPDATE procurement_order
+        SET product_uom = 19
+        WHERE product_uom in (15, 40);
+        """)
+    # SERVICIO, SERVICIO & Servicio to Unit(s) purchase order line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE purchase_order_line
+        SET product_uom = 19
+        WHERE product_uom in (15, 40);
+        """)
+    # SERVICIO, SERVICIO & Servicio to Unit(s) sale order line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE sale_order_line
+        SET product_uom = 19
+        WHERE product_uom in (15, 40);
+        """)
+    # SERVICIO, SERVICIO & Servicio to Unit(s) stock inventory line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE stock_inventory_line
+        SET product_uom = 19
+        WHERE product_uom in (15, 40);
+        """)
+    # SERVICIO, SERVICIO & Servicio to Unit(s) stock move
+    openupgrade.logged_query(
+        cr, """
+        UPDATE stock_move
+        SET product_uom = 19
+        WHERE product_uom in (15, 40);
+        """)
+    # Juegos to Juego product_template
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uom_id = 30
+        WHERE uom_id in (14, 20);
+        """)
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uos_id = 30
+        WHERE uos_id in (14, 20);
+        """)
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uom_po_id = 30
+        WHERE uom_po_id in (14, 20);
+        """)
+     # Juegos to Juego procurement_order
+    openupgrade.logged_query(
+        cr, """
+        UPDATE procurement_order
+        SET product_uom = 30
+        WHERE product_uom in (14, 20);
+        """)
+    # Juegos to juego purchase order line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE purchase_order_line
+        SET product_uom = 30
+        WHERE product_uom in (14, 20);
+        """)
+    # Juegos to juego sale order line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE sale_order_line
+        SET product_uom = 30
+        WHERE product_uom in (14, 20);
+        """)
+    # Juegos to juego stock inventory line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE stock_inventory_line
+        SET product_uom = 30
+        WHERE product_uom in (14, 20);
+        """)
+    # Juegos to juego stock move
+    openupgrade.logged_query(
+        cr, """
+        UPDATE stock_move
+        SET product_uom = 30
+        WHERE product_uom in (14, 20);
+        """)
+    # Herramienta   product_template
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uom_id = 44
+        WHERE uom_id = 43;
+        """)
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uos_id = 44
+        WHERE uos_id = 43;
+        """)
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uom_po_id = 44
+        WHERE uom_po_id = 43;
+        """)
+     # Herramienta  procurement_order
+    openupgrade.logged_query(
+        cr, """
+        UPDATE procurement_order
+        SET product_uom = 44
+        WHERE product_uom = 43;
+        """)
+    # Herramienta   purchase order line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE purchase_order_line
+        SET product_uom = 44
+        WHERE product_uom = 43;
+        """)
+    # Herramienta   sale order line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE sale_order_line
+        SET product_uom = 44
+        WHERE product_uom = 43;
+        """)
+    # Herramienta   stock inventory line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE stock_inventory_line
+        SET product_uom = 44
+        WHERE product_uom = 43;
+        """)
+    # Herramienta   stock move
+    openupgrade.logged_query(
+        cr, """
+        UPDATE stock_move
+        SET product_uom = 44
+        WHERE product_uom = 43;
+        """)
+    # Litros product_template
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uom_id = 11
+        WHERE uom_id = 33;
+        """)
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uos_id = 11
+        WHERE uos_id = 33;
+        """)
+    openupgrade.logged_query(
+        cr, """
+        UPDATE product_template
+        SET uom_po_id = 11
+        WHERE uom_po_id = 33;
+        """)
+     # Litrosprocurement_order
+    openupgrade.logged_query(
+        cr, """
+        UPDATE procurement_order
+        SET product_uom = 11
+        WHERE product_uom = 33;
+        """)
+    # Litros purchase order line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE purchase_order_line
+        SET product_uom = 11
+        WHERE product_uom = 33;
+        """)
+    # Litros sale order line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE sale_order_line
+        SET product_uom = 11
+        WHERE product_uom = 33;
+        """)
+    # Litros stock inventory line
+    openupgrade.logged_query(
+        cr, """
+        UPDATE stock_inventory_line
+        SET product_uom = 11
+        WHERE product_uom = 33;
+        """)
+    # Litros stock move
+    openupgrade.logged_query(
+        cr, """
+        UPDATE stock_move
+        SET product_uom = 11
+        WHERE product_uom = 33;
+        """)
+    # Delete Unused Uoms 
+    openupgrade.logged_query(
+        cr, """
+        DELETE FROM product_uom
+        WHERE id in (16, 41, 42, 15, 40, 14, 20, 43, 33);
+        """)
+    cr.execute(
+    str("""SELECT sil.id, sil.product_id, pc.id category_id FROM stock_inventory_line sil
+        JOIN product_uom pu ON pu.id = sil.product_uom
+        JOIN product_uom_categ pc ON pc.id = pu.category_id"""))
+    data = cr.fetchall()
+    for rec in data:
+        cr.execute(str(
+            """SELECT pc.id category_id, pt.uom_id FROM stock_inventory_line sil
+            JOIN product_product pp ON pp.id = sil.product_id
+            JOIN product_template pt ON pt.id = pp.product_tmpl_id
+            JOIN product_uom pu ON pu.id = pt.uom_id
+            JOIN product_uom_categ pc ON pc.id = pu.category_id
+            WHERE sil.id = %s""") % (rec[0]))
+        line = cr.fetchall()
+        if line[0][0] != rec[2]:
+            cr.execute(str("""
+                UPDATE stock_inventory_line
+                SET product_uom = %s
+                WHERE id = %s""") % (line[0][1], rec[0]))
+    cr.execute(
+    str("""SELECT sm.id, sm.product_id, pc.id category_id FROM stock_move sm
+        JOIN product_uom pu ON pu.id = sm.product_uom
+        JOIN product_uom_categ pc ON pc.id = pu.category_id"""))
+    data2 = cr.fetchall()
+    for rec in data2:
+        cr.execute(str(
+            """SELECT pc.id category_id, pt.uom_id FROM stock_move sm
+            JOIN product_product pp ON pp.id = sm.product_id
+            JOIN product_template pt ON pt.id = pp.product_tmpl_id
+            JOIN product_uom pu ON pu.id = pt.uom_id
+            JOIN product_uom_categ pc ON pc.id = pu.category_id
+            WHERE sm.id = %s""") % (rec[0]))
+        line = cr.fetchall()
+        if line[0][0] != rec[2]:
+            cr.execute(str("""
+                UPDATE stock_move
+                SET product_uom = %s
+                WHERE id = %s""") % (line[0][1], rec[0]))
     cr.execute('drop view if exists report_document_user cascade')
 
     openupgrade.update_module_names(
